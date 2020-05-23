78e9ad41df28f092d20aa9a7b59fd93671afaff1
FarhanAli325706 <farhanlala706@gmail.com>
2020-05-23 14:45:04 +0500

    Project-Start

---
+++ src/main/java/com/example/thymeleafapp/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/Home")
+    public String test()
+    {
+        return "index";
+    }
+}
