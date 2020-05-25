f63c85f8371392d63e2855b43df3a877f3095c20
FarhanAli325706 <farhanlala706@gmail.com>
2020-05-25 18:09:41 +0500

    login complete

--- src/main/java/com/example/thymeleafapp/HomeController.java
+++ src/main/java/com/example/thymeleafapp/HomeController.java
@@ -1,18 +1,29 @@
 package com.example.thymeleafapp;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @Autowired
+    UserService userService;
     @RequestMapping("/Welcome")
     public String homePage()
     {
         return "WelcomePage";
     }
-    @RequestMapping("/index")
-    public String index()
+    @RequestMapping("/login")
+    public String signup()
     {
-        return "index";
+        return "login";
+    }
+    @RequestMapping("/getuser")
+    public String getUser(@RequestParam("email") String email, @RequestParam("password") String password)
+    {
+        UserModel result=userService.loginUser(email,password);
+        return "WelcomePage";
     }
 }
---
+++ src/main/java/com/example/thymeleafapp/UserService.java
@@ -0,0 +1,39 @@
+package com.example.thymeleafapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    UserModel loginUser(String email, String password)
+    {
+        int flag=0;
+        List<UserModel> listOfUser=userRepository.findAll();
+        UserModel resultanUser=new UserModel();
+        for (UserModel u : listOfUser)
+        {
+            if(u.getEmail().equals(email) && u.getUserpassword().equals(password))
+            {
+                resultanUser.setUserid(u.getUserid());
+                resultanUser.setUsername(u.getUsername());
+                resultanUser.setUserpassword(u.getUserpassword());
+                resultanUser.setEmail(u.getEmail());
+                resultanUser.setGender(u.getGender());
+                resultanUser.setBirthdate(u.getBirthdate());
+                resultanUser.setCity(u.getCity());
+                resultanUser.setReligion(u.getReligion());
+                resultanUser.setCountry(u.getCountry());
+                resultanUser.setUsercast(u.getUsercast());
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1)
+            return resultanUser;
+        return null;
+    }
+}
